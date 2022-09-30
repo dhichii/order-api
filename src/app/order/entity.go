623fa373c1f25e57c77dcb390ce553c622caff16
Adhicitta Masran <adhicittamasran@gmail.com>
2022-09-30 15:05:44 +0800

    feat(order): add repo and service interfaces

---
+++ src/app/order/entity.go
@@ -0,0 +1,22 @@
+package order
+
+import (
+	"order-api/src/app/order/handler/response"
+	"order-api/src/app/order/repository/record"
+)
+
+type Repository interface {
+	CreateOrder(data record.Order) error
+	GetOrders() ([]response.Order, error)
+	UpdateOrder(id int, data record.Order) error
+	UpdateOrderItems(orderID int, orderItems []record.Item) error
+	DeleteOrder(id int) error
+	DeleteOrderItems(orderID int) error
+}
+
+type Service interface {
+	CreateOrder(data record.Order) error
+	GetOrders() ([]response.Order, error)
+	UpdateOrder(id int, data record.Order) error
+	DeleteOrder(id int) error
+}
