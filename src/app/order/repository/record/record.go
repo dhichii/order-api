233b389ee608cf9d2febeaf9b7969d29c5c4f447
Adhicitta Masran <adhicittamasran@gmail.com>
2022-09-30 15:00:58 +0800

    feat(order): add response mapper functions

--- src/app/order/repository/record/record.go
+++ src/app/order/repository/record/record.go
@@ -1,5 +1,7 @@
 package record
 
+import "order-api/src/app/order/handler/response"
+
 type Order struct {
 	OrderID      int    `gorm:"primaryKey"`
 	CustomerName string `gorm:"type:varchar"`
@@ -13,4 +15,36 @@ type Item struct {
 	Description string
 	Quantity    int
 	OrderID     int
+}
+
+func MapToBatchResponse(records []Order) (responses []response.Order) {
+	for _, order := range records {
+		responses = append(responses, MapToResponse(order))
+	}
+	return
+}
+
+func MapToResponse(record Order) response.Order {
+	return response.Order{
+		OrderID:      record.OrderID,
+		CustomerName: record.CustomerName,
+		OrderedAt:    record.OrderedAt,
+		Items:        MapItemRecords(record.Items),
+	}
+}
+
+func MapItemRecords(records []Item) (items []response.Item) {
+	for _, item := range records {
+		items = append(items, MapItemRecord(item))
+	}
+	return
+}
+
+func MapItemRecord(item Item) response.Item {
+	return response.Item{
+		ItemID:      item.ItemID,
+		ItemCode:    item.ItemCode,
+		Description: item.Description,
+		Quantity:    item.Quantity,
+	}
 }
