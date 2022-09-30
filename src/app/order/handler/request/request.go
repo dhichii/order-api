fda8ce4698d925d11f8db610007ad6883d3ef730
Adhicitta Masran <adhicittamasran@gmail.com>
2022-09-30 15:01:43 +0800

    feat(order): add record mapper functions

--- src/app/order/handler/request/request.go
+++ src/app/order/handler/request/request.go
@@ -1,5 +1,9 @@
 package request
 
+import (
+	"order-api/src/app/order/repository/record"
+)
+
 type Request struct {
 	OrderedAt    string        `json:"orderedAt"`
 	CustomerName string        `json:"customerName"`
@@ -10,4 +14,27 @@ type ItemRequest struct {
 	ItemCode    string `json:"itemCode"`
 	Description string `json:"description"`
 	Quantity    int    `json:"quantity"`
+}
+
+func (r Request) MapToRecord() record.Order {
+	return record.Order{
+		OrderedAt:    r.OrderedAt,
+		CustomerName: r.CustomerName,
+		Items:        mapItemsRequest(r.Items),
+	}
+}
+
+func mapItemsRequest(request []ItemRequest) (result []record.Item) {
+	for _, item := range request {
+		result = append(result, mapItemRequest(item))
+	}
+	return
+}
+
+func mapItemRequest(item ItemRequest) record.Item {
+	return record.Item{
+		ItemCode:    item.ItemCode,
+		Description: item.Description,
+		Quantity:    item.Quantity,
+	}
 }
